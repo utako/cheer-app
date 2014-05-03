35a94cf25ee152de34930a889f42b8660ec5e4f8
Utako Ueda <utakoueda@gmail.com>
2014-05-03 12:31:07 -0700

    edit comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,8 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.commenter = current_user
-    @comment.commentable_id = commented_item.id
-    @comment.commentable_type = commented_item.class
-    url = "#{commented_item.class.to_s.downcase}_url(#{commented_item})"
     if @comment.save
-      redirect_to root_url
+      redirect_to commented_item
     else
       flash[:errors] = @comment.errors.full_messages
       redirect_to root_url
@@ -27,9 +24,9 @@ class CommentsController < ApplicationController
   private
 
   def commented_item
-    if comment_params[:commentable_type] == Goal
+    if comment_params[:commentable_type] == "Goal"
       Goal.find(comment_params[:commentable_id])
-    elsif comment_params[:commentable_type] == User
+    elsif comment_params[:commentable_type] == "User"
       User.find(comment_params[:commentable_id])
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'goals#index'
   resources :users, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:comment, :create, :destroy]
+    resources :comments, only: [:new, :create, :destroy]
   end
   resource :session, only: [:new, :create, :destroy]
   resources :goals do
-    resources :comments, only: [:comment, :create, :destroy]
+    resources :comments, only: [:new, :create, :destroy]
   end
 end
 
